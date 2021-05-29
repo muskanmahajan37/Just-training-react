cbe983f422b3fefcc2faf10fc417d51da8042944
VictorLira <victorliradev@gmail.com>
2021-05-28 22:05:04 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import MovieWrapper from './components/MovieWrapper';
-import Header from './components/Header';
-import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MovieWrapper />
-      <Footer />
+      <p>hello world</p>
     </div>
   );
 }
