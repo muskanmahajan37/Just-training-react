aacb57452cfecab246079b80600f45f90eeeb855
VictorLira <victorliradev@gmail.com>
2021-05-30 12:48:11 -0300

    year

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -6,14 +6,23 @@ const FormComponent = props => {
 
     let SubmitHandler = function (e) {
         e.preventDefault();
-        console.log('ok')
+        
+
     }
 
     return (
         <form onClick={ SubmitHandler }>
             <div className="formControl">
-                <label style={{color: !userValues ? 'red' : 'black'} } for="firstName"> first Name </label>
-                <input type="text" placeholder="digite" id="firstName" onChange={SubmitHandler}
+                <label
+                    style={{ color: !userValues ? 'red' : 'black' }}
+                    for="firstName"> first Name
+                </label>
+                <input
+                    style={{borderColor: !userValues ? 'red' : 'black'}}
+                    type="text"
+                    placeholder="digite"
+                    id="firstName"
+                    onChange={SubmitHandler}
                 />
             </div>
             <Button>Enviar</Button>
