0b40f8a2ea1669c8991666f301e1ba0f9f439c7e
VictorLira <victorliradev@gmail.com>
2021-06-10 11:40:36 -0300

    using classes

--- src/components/FormControl.js
+++ src/components/FormControl.js
@@ -5,7 +5,7 @@ function FormControl(props) {
     let isValid = props.booleano;
 
     return (
-        <div className="form-control">
+        <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
             <label for={props.id}>{props.children}</label>
             <input
                 type={props.type}
@@ -13,12 +13,6 @@ function FormControl(props) {
                 name={props.name}
                 placeholder={props.placeHolder}
                 onChange={props.onUpdateUsernameInfo}
-                style={
-                    {
-                        borderColor: !isValid ? "red" : "black",
-                        backgroundColor: !isValid ? 'rgb(247, 235, 235)' : 'white'
-                    }
-                }
             ></input>
             <ErrorMessages validation={props.booleano} ErrorMessages={props.outputMessages} />
         </div>
