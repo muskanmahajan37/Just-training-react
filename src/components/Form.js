b0e2ec8667795e87c607ceecb9b99ba779ff6ff2
VictorLira <victorliradev@gmail.com>
2021-06-10 17:52:28 -0300

    email validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,66 +1,100 @@
 import "./Form.css";
 import FormControl from "./FormControl";
-import {useState} from 'react';
+import { useState } from "react";
 import Button from "./Button";
 
 function Form() {
     const [usernameInfo, setUsernameInfo] = useState({
-        usernameInput: '',
-        usernameErrorMessage: '',
-        isUsernameValid: true
+        usernameInput: "",
+        usernameErrorMessage: "",
+        isUsernameValid: true,
+    });
+
+    const [emailInfo, setEmailInfo] = useState({
+        emailInput: '',
+        emailErrorMessage: '',
+        isEmailValid: true
     })
 
     function hasWhiteSpace(s) {
-        return s.indexOf(' ') >= 0;
-      }
+        return s.indexOf(" ") >= 0;
+    }
 
-    function updateUsernameInfo(e) {
+    function onUsernameInfo(e) {
         e.preventDefault();
 
         setUsernameInfo((usernameValues) => {
             const currentUsernameInfo = {
                 ...usernameValues,
                 usernameInput: e.target.value,
-            }
+            };
             return currentUsernameInfo;
-        })
+        });
 
-        if (usernameInfo.usernameInput.trim().length <= 5 ) {
+        if (usernameInfo.usernameInput.trim().length <= 5) {
             setUsernameInfo((usernameValues) => {
                 const currentUsernameInfo = {
                     ...usernameValues,
-                    usernameErrorMessage: 'Short username',
-                    isUsernameValid: false
-                }
-                return currentUsernameInfo
-            })
-
+                    usernameErrorMessage: "Short username",
+                    isUsernameValid: false,
+                };
+                return currentUsernameInfo;
+            });
         } else if (hasWhiteSpace(usernameInfo.usernameInput) === true) {
             setUsernameInfo((usernameValues) => {
                 const currentUsernameValues = {
                     ...usernameValues,
-                    usernameErrorMessage: 'Remove white space',
-                    isUsernameValid: false
-                }
+                    usernameErrorMessage: "Remove white space",
+                    isUsernameValid: false,
+                };
                 return currentUsernameValues;
-            })
-        }  else {
+            });
+        } else {
             setUsernameInfo((usernameValues) => {
                 const currentUsernameInfo = {
                     ...usernameValues,
-                    isUsernameValid: true
-                }
-                return currentUsernameInfo
-            })
+                    isUsernameValid: true,
+                };
+                return currentUsernameInfo;
+            });
         }
     }
 
+    function onEmailInfo(e) {
+        e.preventDefault()
+
+        setEmailInfo((emailValues) => {
+            const currentEmailInfo = {
+                ...emailValues,
+                emailInput: e.target.value
+            }
+            return currentEmailInfo;
+        })
+
+        if (emailInfo.emailInput.length <= 5) {
+            setEmailInfo((emailValues) => {
+                const currentEmailInfo = {
+                    ...emailValues,
+                    isEmailValid: false,
+                    emailErrorMessage: 'invalid E-mail address'
+                }
+                return currentEmailInfo;
+            })
+        } else {
+            setEmailInfo((emailValues) => {
+                const currentEmailInfo = {
+                    ...emailValues,
+                    isEmailValid: true
+                }
+                return currentEmailInfo;
+            })
+        }
+
+    }
 
     function submitHandler(e) {
         e.preventDefault();
-        
     }
-
 
     return (
         <form>
@@ -72,7 +106,7 @@ function Form() {
                 booleano={usernameInfo.isUsernameValid}
                 placeHolder="Username"
                 outputMessages={usernameInfo.usernameErrorMessage}
-                onChange={updateUsernameInfo}
+                onChange={onUsernameInfo}
             >
                 Username
             </FormControl>
@@ -81,8 +115,10 @@ function Form() {
                 id="email"
                 type="email"
                 name="email"
+                booleano={emailInfo.isEmailValid}
                 placeHolder="example@gmail.com"
-                outputMessages="haha"
+                outputMessages={emailInfo.emailErrorMessage}
+                onChange={onEmailInfo}
             >
                 E-mail
             </FormControl>
