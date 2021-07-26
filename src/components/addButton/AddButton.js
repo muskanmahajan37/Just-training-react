2fa77a2a6c6207df4851060b7b34cc17e8f80264
VictorLira <victorliradev@gmail.com>
2021-07-25 21:40:52 -0300

    rendering list of users

---
+++ src/components/addButton/AddButton.js
@@ -0,0 +1,11 @@
+import styles from './AddButton.module.css';
+
+const AddButton = function () {
+    return (
+        <button className={styles.AdicionarButton}>
+            Adicionar
+        </button>
+    )
+}
+
+export default AddButton
--- src/components/userList/UserList.js
+++ src/components/userList/UserList.js
@@ -1,114 +1,21 @@
-import "./styles/UserList.css";
-import React, { useState } from "react";
-import PopUp from "../popUp/PopUp";
+import styles from "./UserList.module.css";
+import AddButton from "../addButton/AddButton";
 
-const userAccounts = [
-    {
-        nome: "Silas Calebe",
-        seguidores: "45",
-        municipio: "Cariacica",
-        faculdade: "Multivix",
-        ocupacao: "Advogado",
-        img: "./user-1.jpg",
-        id: "n1",
-        add: "Adicionar",
-        cancelar: "cancelar",
-    },
-    {
-        nome: "La Voipe",
-        seguidores: "100",
-        municipio: "Vitória",
-        faculdade: "EEEFM. Dr josé Moysés",
-        profissao: "estudante",
-        img: "./user-2.jpg",
-        id: "n2",
-        add: "Adicionar",
-        cancelar: "cancelar",
-    },
-    {
-        nome: "Brenda Ker",
-        seguidores: "600",
-        municipio: "Vila velha",
-        faculdade: "EEEFM. Dr Maria Augusta Tavares",
-        profissao: "Programador",
-        img: "./user-3.jpg",
-        id: "n3",
-        add: "Adicionar",
-        cancelar: "cancelar",
-    },
-    {
-        nome: "Jhayane Kelly",
-        seguidores: "56",
-        municipio: "Serra",
-        faculdade: "UFES",
-        profissao: "Medicina Veterinária",
-        img: "./user-4.jpg",
-        id: "n4",
-        add: "Adicionar",
-        cancelar: "cancelar",
-    },
-];
-
-const UserList = (props) => {
-    const [showPopUp, setShowPopUp] = useState();
-    const [currentPopUp, setCurrentPopUp] = useState([]);
-
-    // const [conectionRequest, sendConectionRequest] = useState(true);
-
-    const displayPopUp = (e) => {
-        const newItem = userAccounts.filter((acc) => acc.id === e.target.id);
-        setCurrentPopUp(...newItem);
-        setShowPopUp(true);
-    };
-
-    const mouseLeave = function () {
-        setShowPopUp(false);
-    };
-
-    const AddFriend = function (e) {
-        e.preventDefault();
-
-        setCurrentPopUp((currentPopUp.add = "cancelar"));
-    };
-
+const UserList = function (props) {
     return (
-        <ul className="user_list">
-            {userAccounts.map((acc) => {
+        <ul className={styles.user_list}>
+            {props.item.map((acc) => {
                 return (
-                    <li className="user_item" key={acc.id}>
-                        {currentPopUp.id === acc.id && showPopUp && (
-                            <PopUp
-                                nome={currentPopUp.nome}
-                                img={currentPopUp.img}
-                                id={currentPopUp.id}
-                                adicionarText={currentPopUp.add}
-                                seguidores={currentPopUp.seguidores}
-                                municipio={currentPopUp.municipio}
-                                key={currentPopUp.id}
-                                onAddFriend={AddFriend}
-                                onMouseLeave={mouseLeave}
-                            />
-                        )}
-                        <div className="picture">
-                            <img src={acc.img}></img>
+                    <li className={styles.list_item}>
+                        <div className={styles.profile}>
+                            <div className={styles.picture}>
+                                <img src="./user-1.jpg"></img>
+                            </div>
+                            <span>{acc.nome}</span>
+                            <span>{acc.profissao}</span>
+                            <span>mora em {acc.municipio}</span>
                         </div>
-                        <div className="user_informations">
-                            <span
-                                className="username"
-                                onMouseEnter={displayPopUp}
-                                id={acc.id}
-                            >
-                                {acc.nome}
-                            </span>
-                            <p className="seguidores">
-                                {acc.seguidores} amigos em comum
-                            </p>
-                            <p className="municipio">{acc.municipio}</p>
-                            <p className="faculdade">
-                                estudante em {acc.faculdade}
-                            </p>
-                            <p className="ocupacao">{acc.ocupacao}</p>
-                        </div>
+                        <AddButton />
                     </li>
                 );
             })}
