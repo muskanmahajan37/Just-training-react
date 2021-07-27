7bd74f9d64493f8b0d27da2b10f4a8f57f7a3230
VictorLira <victorliradev@gmail.com>
2021-07-27 14:50:21 -0300

    cleaning up everything

--- src/App.js
+++ src/App.js
@@ -1,56 +1,7 @@
 import React, { useState } from "react";
-import AddNewUser from "./components/addNewUser/AddNewUser";
-import UserList from "./components/userList/UserList";
+
 const App = () => {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredProfissao, setEnteredProfissao] = useState("");
-    const [enteredMunicipio, setEnteredMunicipio] = useState("");
-    const [userList, setUserList] = useState([]);
-
-    const changeUsername = function (e) {
-        setEnteredUsername(e.target.value);
-    };
-
-    const changeProfissao = function (e) {
-        setEnteredProfissao(e.target.value);
-    };
-
-    const changeMunicipio = function (e) {
-        setEnteredMunicipio(e.target.value);
-    };
-
-    const createAccount = function (e) {
-        e.preventDefault();
-        setUserList((prevList) => {
-            const previous = [...prevList];
-            previous.unshift({
-                nome: enteredUsername,
-                profissao: enteredProfissao,
-                municipio: enteredMunicipio
-            });
-            console.log(userList)
-            return previous;
-        });
-    };
-
-    return (
-        <>
-            <div className="wrapper">
-                <AddNewUser
-                    onChangeUsername={changeUsername}
-                    onChangeProfissao={changeProfissao}
-                    onChangeMunicio={changeMunicipio}
-                    onSubmit={createAccount}
-                />
-            </div>
-
-            <div className="account_wrapper">
-                <UserList
-                    item={userList}
-                />
-            </div>
-        </>
-    );
+    return <></>;
 };
 
 export default App;
