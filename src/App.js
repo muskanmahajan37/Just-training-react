c373987d1781ce2e6a7d76173402dccdb7925dee
VictorLira <victorliradev@gmail.com>
2021-07-27 22:18:43 -0300

    react hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Counter from "./components/counter/Counter";
 
 const App = () => {
-    return <></>;
+    const [isValid, setIsValid] = useState(true);
+
+    setTimeout(() => {
+        setIsValid(false)
+    }, 5000)
+
+    return (
+        <React.Fragment>
+            {isValid && <Counter />}
+        </React.Fragment>
+    );
 };
 
 export default App;
